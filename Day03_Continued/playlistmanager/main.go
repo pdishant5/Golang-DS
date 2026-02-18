1bb75252b15ef9bf1e7eb9bb553345c8137cd006
Dishant Patel <pateldishant555@gmail.com>
2026-02-18 18:22:53 +0530

    implements the merging playlists function in different ways

--- Day03_Continued/playlistmanager/main.go
+++ Day03_Continued/playlistmanager/main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"playlistmanager/playlist"
 	"playlistmanager/songnode"
 )
 
 func main() {
-	playlist := playlist.New("My Playlist")
+	playlist1 := playlist.New("My Playlist")
 	newSong := songnode.New("Shiva Tandava Stotram", "Shankar Mahadevan", 300)
-	playlist.AddSongAtFront(newSong)
-	playlist.Traverse()
-	playlist.AddSongAtFront(songnode.New("Nagar Nandji Na Lal", "Aditya Gadhvi", 200))
-	playlist.Traverse()
+	playlist1.AddSongAtFront(newSong)
+	// playlist1.Traverse()
+	playlist1.AddSongAtFront(songnode.New("Nagar Nandji Na Lal", "Aditya Gadhvi", 200))
+	// playlist1.Traverse()
 
-	playlist.AddSongAtEnd(songnode.New("Maa Tujhe Salaam", "A.R. Rahman", 240))
-	// playlist.Traverse()
-	playlist.AddSongAtEnd(songnode.New("Goti Lo", "Aditya Gadhvi", 180))
-	// playlist.Traverse()
-	playlist.AddSongAtEnd(songnode.New("Krishn Bhagwan Halya", "Aditya Gadhvi", 210))
-	playlist.Traverse()
+	playlist1.AddSongAtEnd(songnode.New("Maa Tujhe Salaam", "A.R. Rahman", 240))
+	// playlist1.Traverse()
+	playlist1.AddSongAtEnd(songnode.New("Goti Lo", "Aditya Gadhvi", 180))
+	// playlist1.Traverse()
+	playlist1.AddSongAtEnd(songnode.New("Krishn Bhagwan Halya", "Aditya Gadhvi", 210))
+	// playlist1.Traverse()
 
-	playlist.DeleteSong("Goti Lo")
-	playlist.Traverse()
+	playlist1.DeleteSong("Goti Lo")
+	playlist1.Traverse()
+
+	playlist2 := playlist.New("Your Playlist")
+	newSong = songnode.New("Bhajan 1", "Artist 1", 200)
+	playlist2.AddSongAtFront(newSong)
+	// playlist2.Traverse()
+	playlist2.AddSongAtFront(songnode.New("Bhajan 2", "Artist 2", 250))
+	// playlist2.Traverse()
+
+	playlist2.AddSongAtEnd(songnode.New("Bhajan 3", "Artist 1", 240))
+	// playlist2.Traverse()
+	playlist2.AddSongAtFront(songnode.New("Bhajan 4", "Artist 3", 280))
+	// playlist2.Traverse()
+	playlist2.AddSongAtEnd(songnode.New("Bhajan 5", "Artist 3", 310))
+	playlist2.AddSongAtFront(songnode.New("Bhajan 6", "Artist 1", 230))
+	playlist2.AddSongAtEnd(songnode.New("Bhajan 7", "Artist 3", 210))
+	playlist2.Traverse()
+
+	// playlist2.DeleteSong("Goti Lo")
+	// playlist2.Traverse() // same list as the previous, because we deleted the song that was not in the list
+
+	// merging two playlists by appending one list at the tail of the other
+	t1 := time.Now()
+	mergedPlaylist1 := playlist.MergePlaylists1(playlist1, playlist2)
+	fmt.Println("Time taken in method 1:", time.Since(t1))
+	mergedPlaylist1.Traverse()
+
+	// niling bothe the playlists to ensure that they will not be used anymore and the merged playlist is a new list
+	playlist1, playlist2 = nil, nil
+
+	// merging two playlists by alternating the songs from both the lists
+	// mergedPlaylist2 := playlist.MergePlaylists2(playlist1, playlist2)
+	// t1 := time.Now()
+	// mergedPlaylist2 := playlist.MergePlaylists2(playlist2, playlist1)
+	// fmt.Println("Time taken in method 2:", time.Since(t1))
+	// mergedPlaylist2.Traverse()
+	// playlist1, playlist2 = nil, nil
 }
--- Day03_Continued/playlistmanager/playlist/playlist.go
+++ Day03_Continued/playlistmanager/playlist/playlist.go
@@ -6,17 +6,19 @@ import (
 )
 
 type PlayList struct {
-	Name     string
-	Duration int
+	Name       string
+	Duration   int
+	TotalSongs int
 
 	head *songnode.SongNode
 }
 
 func New(name string) *PlayList {
 	return &PlayList{
-		Name:     name,
-		Duration: 0,
-		head:     nil,
+		Name:       name,
+		Duration:   0,
+		TotalSongs: 0,
+		head:       nil,
 	}
 }
 
@@ -24,6 +26,7 @@ func (pl *PlayList) AddSongAtEnd(song *songnode.SongNode) {
 	if pl.head == nil {
 		pl.head = song
 		pl.Duration = song.Duration
+		pl.TotalSongs++
 		return
 	}
 
@@ -33,6 +36,7 @@ func (pl *PlayList) AddSongAtEnd(song *songnode.SongNode) {
 	}
 	current.Next = song
 	pl.Duration += song.Duration
+	pl.TotalSongs++
 }
 
 func (pl *PlayList) AddSongAtFront(song *songnode.SongNode) {
@@ -40,6 +44,7 @@ func (pl *PlayList) AddSongAtFront(song *songnode.SongNode) {
 	pl.head = song
 
 	pl.Duration += song.Duration
+	pl.TotalSongs++
 }
 
 func (pl *PlayList) DeleteSong(name string) {
@@ -50,6 +55,7 @@ func (pl *PlayList) DeleteSong(name string) {
 	if pl.head.Name == name {
 		pl.Duration -= pl.head.Duration
 		pl.head = pl.head.Next
+		pl.TotalSongs--
 		return
 	}
 
@@ -57,6 +63,7 @@ func (pl *PlayList) DeleteSong(name string) {
 	for current.Next != nil {
 		if current.Next.Name == name {
 			pl.Duration -= current.Next.Duration
+			pl.TotalSongs--
 			current.Next = current.Next.Next
 			return
 		}
@@ -67,11 +74,73 @@ func (pl *PlayList) DeleteSong(name string) {
 func (pl *PlayList) Traverse() {
 	current := pl.head
 	i := 1
-	fmt.Printf("Listing playlist: %s; Total Duration: %d seconds.\n\n", pl.Name, pl.Duration)
+	fmt.Printf("Listing playlist: %s; Total Songs: %d; Total Duration: %d seconds.\n\n", pl.Name, pl.TotalSongs, pl.Duration)
 	for current != nil {
 		fmt.Printf("Song %d:\nName: %s; Artist: %s; Duration: %d seconds\n\n", i, current.Name, current.Artist, current.Duration)
 
 		current = current.Next
 		i++
 	}
+}
+
+// merges two playlists by appending the longer one at the end of the shorter one in termas of number pf songs
+func MergePlaylists1(pl1, pl2 *PlayList) *PlayList {
+	mergedPlaylist := New(pl1.Name + " + " + pl2.Name)
+	mergedPlaylist.Duration = pl1.Duration + pl2.Duration
+	mergedPlaylist.TotalSongs = pl1.TotalSongs + pl2.TotalSongs
+
+	// if playlist 1 is shorter
+	if pl1.TotalSongs <= pl2.TotalSongs {
+		head1 := pl1.head
+		mergedPlaylist.head = pl1.head
+
+		for head1.Next != nil {
+			head1 = head1.Next
+		}
+
+		head1.Next = pl2.head
+		return mergedPlaylist
+	}
+
+	// if playlist 2 is shorter
+	head2 := pl2.head
+	mergedPlaylist.head = pl2.head
+
+	for head2.Next != nil {
+		head2 = head2.Next
+	}
+
+	head2.Next = pl1.head
+	return mergedPlaylist
+}
+
+func MergePlaylists2(pl1, pl2 *PlayList) *PlayList {
+	mergedPlaylist := New(pl1.Name + " + " + pl2.Name)
+	mergedPlaylist.head = pl1.head
+	mergedPlaylist.Duration = pl1.Duration + pl2.Duration
+	mergedPlaylist.TotalSongs = pl1.TotalSongs + pl2.TotalSongs
+
+	head1 := pl1.head
+	head2 := pl2.head
+
+	for head1.Next != nil && head2.Next != nil {
+		headNext := head2.Next
+		head2.Next = head1.Next
+		head1.Next = head2
+
+		head1 = head1.Next.Next
+		head2 = headNext
+	}
+
+	// if playlist 1 is exhausted
+	if head1.Next == nil {
+		head1.Next = head2
+		return mergedPlaylist
+	}
+
+	// if playlist 2 is exhausted
+	head2.Next = head1.Next
+	head1.Next = head2
+	return mergedPlaylist
+
 }
